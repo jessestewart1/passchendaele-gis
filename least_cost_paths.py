3fbbc5ef46e26a7469e2093b27a768ab645119ed
Jesse Stewart <jesse-stewart18@hotmail.com>
2024-10-23 08:42:09 -0300

    [1] added chunked least cost path calculation

--- least_cost_paths.py
+++ least_cost_paths.py
@@ -88,14 +88,16 @@ class LeastCostPaths:
 
         # Batch process least-cost path calculation using specific chunk size.
         chunksize = 100
-        /
+        for idx in tqdm(range(int(len(self.results) / chunksize) + 1)):
+            flag = (self.results.index >= (idx * chunksize)) & (self.results.index < ((idx + 1) * chunksize))
 
-        # Calculate least-cost paths using Dijkstra's algorithm.
-        self.results["indexes"] = self.results[[self.field_index_source, self.field_index_target]].apply(dict, axis=1)\
-            .map(lambda row: self.graph.get_shortest_path(v=row[self.field_index_source],
-                                                          to=row[self.field_index_target],
-                                                          weights=self.field_cost,
-                                                          mode="out", output="vpath", algorithm="dijkstra"))
+            # Calculate least-cost paths using Dijkstra's algorithm.
+            self.results.loc[flag, "indexes"] = self.results.loc[flag, [self.field_index_source,
+                                                                        self.field_index_target]].apply(dict, axis=1)\
+                .map(lambda row: self.graph.get_shortest_path(v=row[self.field_index_source],
+                                                              to=row[self.field_index_target],
+                                                              weights=self.field_cost,
+                                                              mode="out", output="vpath", algorithm="dijkstra"))
 
     def create_graph(self) -> None:
         """Creates a directed Graph from a collection of node indexes and cost values."""
