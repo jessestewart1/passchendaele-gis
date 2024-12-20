2cc76a5863c0cbe141e7aacf6a0fe31e0bd46f82
Jesse Stewart <jesse-stewart18@hotmail.com>
2024-12-20 11:46:11 -0400

    [1] completed and functional implementation of statistical_significance.py

--- src/least_cost_paths/statistical_significance.py
+++ src/least_cost_paths/statistical_significance.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from scipy.linalg import svd
 from scipy.stats import pearsonr
 from sqlalchemy import create_engine
+from tqdm import tqdm
 
 # Set logger.
 logger = logging.getLogger(__name__)
@@ -20,15 +21,15 @@ logger.addHandler(handler)
 class StatisticalSignificance:
     """Defines the StatisticalSignificance class."""
 
-    def __init__(self, src: Path, layer_lcps: str, layer_sps: str, dst_name: str) -> None:
+    def __init__(self, src: Path, layer_lcps: str, layer_sps: str, dst_name: str, permutations: int = 9999) -> None:
         """Initializes the StatisticalSignificance class."""
 
         self.dst = Path(src.parent / dst_name).with_suffix(".csv")
-        self.permutations = 9999
+        self.permutations = permutations
 
         # Compile source data.
         # Note: Not all columns required for statistical significance tests, including geometry, hence reading via SQL.
-        engine = create_engine(f"sqlite:////{src}")
+        engine = create_engine(f"sqlite:///{src}")
         query_fields = ", ".join(f'"{field}"' for field in ("group", "source", "target", "cost", "distance"))
 
         logger.info(f"Compiling source data - least-cost paths: {src}, layer={layer_lcps}.")
@@ -39,7 +40,9 @@ class StatisticalSignificance:
 
         # Configure output DataFrame.
         groups = list(set(self.lcps["group"]))
-        self.pvalues = pd.DataFrame({"group": groups, "pvalue": [-1] * len(groups)})
+        dst_cols = ("pvalue", "r_observed", "n_permuted", "r_permuted_min", "r_permuted_max", "r_permuted_mean",
+                    "r_permuted_abs_min", "r_permuted_abs_max", "r_permuted_abs_mean")
+        self.dst_df = pd.DataFrame({"group": groups, **{col: [-1.0] * len(groups) for col in dst_cols}})
 
     def __call__(self) -> None:
         """Executes the StatisticalSignificance class."""
@@ -47,7 +50,7 @@ class StatisticalSignificance:
         self.calculate_pvalues()
 
         # Export results.
-        self.pvalues.to_csv(self.dst, sep=",", header=True, index=False)
+        self.dst_df.to_csv(self.dst, sep=",", header=True, index=False)
         logger.info(f"Exported results to: {self.dst}.")
 
     @staticmethod
@@ -63,7 +66,7 @@ class StatisticalSignificance:
         logger.info(f"Generating cost-distance matrix using cost field: {cost}.")
 
         # Sort DataFrame to ensure alignment.
-        df.sort_values(by=["group", "source", "target"], ignore_index=True, inplace=True)
+        df.sort_values(by=["source", "target"], ignore_index=True, inplace=True)
 
         # Normalize distance values to account for scaling differences.
         df[cost] = df[cost] / df[cost].max()
@@ -73,54 +76,54 @@ class StatisticalSignificance:
         locations_target = sorted(set(df["target"]))
         matrix = np.full((len(locations_source), len(locations_target)), 0.0)
 
-        # Iteratively populate matrix.
-        for i, source in enumerate(locations_source):
-            for j, target in enumerate(locations_target):
-                matrix[i, j] = df.loc[(df["source"] == source) & (df["target"] == target), cost]
+        # Generate source-target to matrix index lookups.
+        lookup_source_idx = dict(zip(locations_source, range(len(locations_source))))
+        lookup_target_idx = dict(zip(locations_target, range(len(locations_target))))
+
+        # Populate matrix.
+        matrix[df["source"].map(lookup_source_idx), df["target"].map(lookup_target_idx)] = df[cost].values
 
         return matrix
 
-    def _gen_msr_matrices(self, matrix: np.ndarray) -> [np.ndarray, ...]:
+    @staticmethod
+    def _gen_msr_matrix(matrix: np.ndarray) -> np.ndarray:
         """
-        Generates Moran spectral randomization (MSR) matrices using singular value decomposition (SVD).
+        Generates a randomized matrix using Moran spectral randomization (MSR) via singular value decomposition (SVD).
 
         :param np.ndarray matrix: Cost-distance matrix.
-        :return [np.ndarray, ...]: List of randomized cost-distance matrices.
+        :return np.ndarray: Randomized cost-distance matrix.
         """
-
-        logger.info(f"Generating {self.permutations} Moran spectral randomization (MSR) matrices.")
 
         # Decompose cost-distance matrix via singular value decomposition (SVD).
         u, s, vh = svd(matrix, full_matrices=False, compute_uv=True, overwrite_a=False)
 
-        # Generate randomized matrices.
-        matrices_msr = list()
-        for _ in range(self.permutations):
+        # Generate randomized permutation.
+        s_permuted = np.random.permutation(s)
 
-            # Generate randomized permutation.
-            s_permuted = np.random.permutation(s)
+        # Reconstruct as a matrix via dot product.
+        matrix_msr = np.dot(u * s_permuted, vh)
 
-            # Reconstruct as a matrix via dot product.
-            matrix_msr = np.dot(u * s_permuted, vh)
-            matrices_msr.append(matrix_msr)
-
-        return matrices_msr
+        return matrix_msr
 
     def calculate_pvalues(self) -> None:
         """Calculates two-tailed p-values for each group."""
 
         # Iterate groups.
-        for group in set(self.lcps["group"]):
+        for group in sorted(set(self.lcps["group"])):
+
+            logger.info(f"Group: {group}")
+
+            # Subset DataFrame to group.
+            lcps = self.lcps.loc[self.lcps["group"] == group].copy(deep=True)
+            lcps.reset_index(drop=True, inplace=True)
+            sps = self.sps.loc[self.sps["group"] == group].copy(deep=True)
+            sps.reset_index(drop=True, inplace=True)
 
             # Generate cost-distance matrices.
-            flag_group = self.lcps["group"] == group
-            matrix_lcp = self._gen_cd_matrix(self.lcps.loc[flag_group], cost="cost")
-            matrix_sp = self._gen_cd_matrix(self.sps.loc[flag_group], cost="distance")
+            matrix_lcp = self._gen_cd_matrix(lcps, cost="cost")
+            matrix_sp = self._gen_cd_matrix(sps, cost="distance")
 
-            # Generate Moran spectral randomization (MSR) matrices.
-            matrices_msr = self._gen_msr_matrices(matrix_lcp)
-
-            logger.info(f"Calculating p-value for group: {group}.")
+            # Calculate p-value.
 
             # Flatten cost-distance matrices.
             distances_lcp = matrix_lcp.flatten()
@@ -129,9 +132,13 @@ class StatisticalSignificance:
             # Calculate Pearson's correlation coefficient (r) based on the observed data.
             r = pearsonr(distances_lcp, distances_sp, alternative="two-sided")[0]
 
-            # Permute data.
+            # Iterate permutations.
             r_permuted = list()
-            for matrix_msr in matrices_msr:
+            for _ in tqdm(range(self.permutations), desc=f"Generating MSR matrices and calculating Pearson's "
+                                                         f"correlation coefficients (r)"):
+
+                # Generate  MSR matrix.
+                matrix_msr = self._gen_msr_matrix(matrix_lcp)
 
                 # Flatten MSR matrix.
                 distances_msr = matrix_msr.flatten()
@@ -140,10 +147,23 @@ class StatisticalSignificance:
                 r_ = pearsonr(distances_msr, distances_sp, alternative="two-sided")[0]
                 r_permuted.append(r_)
 
-            # Calculate and store p-value (two-tailed).
+            # Calculate p-value (two-tailed).
             r_permuted = np.array(r_permuted)
             pvalue = (np.sum(np.abs(r_permuted) >= np.abs(r)) + 1) / (self.permutations + 1)
-            self.pvalues.loc[self.pvalues["group"] == group, "pvalue"] = pvalue
+
+            # Store results.
+            flag = self.dst_df["group"] == group
+            self.dst_df.loc[flag, "pvalue"] = pvalue
+
+            # Store results - supplementary.
+            self.dst_df.loc[flag, "r_observed"] = r
+            self.dst_df.loc[flag, "n_permuted"] = self.permutations
+            self.dst_df.loc[flag, "r_permuted_min"] = min(r_permuted)
+            self.dst_df.loc[flag, "r_permuted_max"] = max(r_permuted)
+            self.dst_df.loc[flag, "r_permuted_mean"] = np.mean(r_permuted)
+            self.dst_df.loc[flag, "r_permuted_abs_min"] = min(map(abs, r_permuted))
+            self.dst_df.loc[flag, "r_permuted_abs_max"] = max(map(abs, r_permuted))
+            self.dst_df.loc[flag, "r_permuted_abs_mean"] = np.mean(tuple(map(abs, r_permuted)))
 
 
 @click.command()
@@ -151,7 +171,8 @@ class StatisticalSignificance:
 @click.argument("layer_lcps", type=click.STRING)
 @click.argument("layer_sps", type=click.STRING)
 @click.argument("dst_name", type=click.STRING)
-def main(src: Path, layer_lcps: str, layer_sps: str, dst_name: str) -> None:
+@click.argument("permutations", type=click.INT, default=9999)
+def main(src: Path, layer_lcps: str, layer_sps: str, dst_name: str, permutations: int = 9999) -> None:
     """
     \b
     Description: Calculates the two-tailed p-value for each group of least-cost paths from two GeoPackage layers:
@@ -171,11 +192,12 @@ def main(src: Path, layer_lcps: str, layer_sps: str, dst_name: str) -> None:
     :param str layer_lcps: GeoPackage layer containing least-cost paths and their cost values.
     :param str layer_sps: GeoPackage layer containing shortest-paths and their distance values.
     :param str dst_name: Output CSV file name (excluding .csv suffix).
+    :param int permutations: Number of permutations to evaluate the null hypothesis.
     """
 
     try:
 
-        statistical_significance = StatisticalSignificance(src, layer_lcps, layer_sps, dst_name)
+        statistical_significance = StatisticalSignificance(src, layer_lcps, layer_sps, dst_name, permutations)
         statistical_significance()
 
     except KeyboardInterrupt:
